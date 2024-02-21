7dbb1c27d337e293ddc41e860a64789b25a59ac6
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-02-21 20:58:50 +0200

    Add files via upload

---
+++ Responsive.js
@@ -0,0 +1,63 @@
+function notice(){
+alert("The source code is available for free, therefore the author asks for your support, or donation of any form. Also remember to share the code with others.");
+}
+
+//redirection using open() function 
+function documentation(){  
+open("https://salmandascript.github.io/ProfilerDocumentation");
+}
+
+function imageComponent(){  
+open("https://salmandascript.github.io/Profiler1");
+}
+
+function videoComponent(){  
+open("https://salmandascript.github.io/Profiler2");
+}
+
+//function to open portal 
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline");
+}
+
+//function to open help
+function help(){
+alert("This will open Profiler UI Component Documentation.");
+}
+
+//function to open help
+function help2(){
+open("https://salmandascript.github.io/AboutSalmanda/ProfilerDocumentation ");
+}
+
+//function to open contacts
+function contact(){
+open("https://salmandascript.github.io/AboutSalmanda/contact.html");
+}
+
+//function to open Terms of service 
+function Service(){
+open("https://salmandascript.github.io/AboutSalmanda/Service.html");
+}
+
+//function to open About
+function about(){
+open("https://salmandascript.github.io/AboutSalmanda/about.html");
+}
+
+//function to open Capital Cities
+function capitalCities(){
+open("https://salmandascript.github.io/CapitalCities.App/index.html");
+}
+
+function faceBook(){
+open("https://www.facebook.com/profile.php?id=100009564969932");
+}
+
+function telegram(){
+open("https://t.me/Salmandamusiq");
+}
+
+function email(){
+open("mailto: clementjuma.cmm@gmail.com");
+}
